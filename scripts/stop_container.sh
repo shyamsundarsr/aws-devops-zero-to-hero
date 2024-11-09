a3f0662a5f55daf62d8b3b9799589a730f6383ae
shyamsundarsr <68515647+shyamsundarsr@users.noreply.github.com>
2024-11-09 12:54:16 +0530

    updated docker command

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,5 +3,5 @@ set -e
 
 # Stop the running container (if any)
 echo "Stopping docker containers"
-containerID='docker ps | awk -F" " '{Print $1}'
+containerID=$(docker ps | awk 'NR>1 {print $1}')
 docker rm -f $containerID
